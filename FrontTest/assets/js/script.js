6000f09f6433e7ea1306a9cacf8b5c17ee854777
VItaliyBoev <81472884+VItaliyBoev@users.noreply.github.com>
2021-09-21 20:50:25 +0500

    decision

---
+++ FrontTest/assets/js/script.js
@@ -0,0 +1,79 @@
+$(document).ready(function () {
+    $('.form__body').submit(function (e) {
+        e.preventDefault();
+//объявление переменных
+        $('.form__field').removeClass('error');
+        let statusName = false;
+        let statusPhone = false;
+        let statusEmail = false;
+        let message = {
+            empty:'Поля не заполненны',
+            correct:'Поля введены некорректно',
+        };
+        let nameField = $('input[name="name"]');
+        let phoneField = $('input[name="phone"]');
+        let emailField = $('input[name="email"]');
+        let name = nameField.val().trim(),
+            phone = phoneField.val().trim(),
+            email = emailField.val().trim();
+//валидация формы запроса начало--------------------------------
+//валидация на пустые поля
+        let arEmpty = [];
+        if (name === '') {
+            arEmpty.push(nameField);
+        }
+        if(phone === '') {
+            arEmpty.push(phoneField);
+        }
+        if (email === '') {
+            arEmpty.push(emailField);
+        }
+        if (arEmpty.length !== 0){
+            $('.msg').removeClass('none').text(message.empty);
+            arEmpty.forEach(function (item){
+                $(item).addClass('error');
+            });
+        }else {
+//валидация ФИО
+            let regName = /^[а-яА-Я\s-]+$/;
+            if (regName.test(name) === false) {
+                $('.msg').removeClass('none').text(message.correct);
+                $(nameField).addClass('error')
+            }else statusName = true;
+//валидация телефона
+            let  regPhone = /^(\+7|7|8)?[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$/;
+            if (regPhone.test(phone) === false){
+                $('.msg').removeClass('none').text(message.correct);
+                $(phoneField).addClass('error')
+            }else statusPhone = true;
+//валидация email
+            let regEmail = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+            if (regEmail.test(email) === false) {
+                $('.msg').removeClass('none').text(message.correct);
+                $(emailField).addClass('error');
+
+            } else statusEmail = true;
+        }
+//валидация формы запроса конец--------------------------------
+//ajax запрос
+        if (statusName === true && statusEmail === true && statusPhone === true) {
+            $.ajax({
+                type: 'post',
+                url: 'https://60376bfd5435040017722533.mockapi.io/form',
+                data: {
+                    name: name,
+                    phone: phone,
+                    email: email,
+                },
+                success: function (data) {
+                    console.log(data);
+                    $('.msg').removeClass('none').text('Форма успешно отправленна');
+                },
+                error: function (xhr, ajaxOptions, thrownError) {
+                    $('.msg').removeClass('none').text('Ошибка: ' + xhr.status + ' ' + thrownError  + ' (запрос не отправлен)');
+                },
+
+            });
+        }
+    });
+});
---
+++ FrontTest/index.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Форма регистрации</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <div class="form">
+        <form class="form__body">
+            <h1 class="form__title">Задать вопрос</h1>
+            <div class="form__fields">
+                <div class="div_form">
+                    <input class="form__field" name="name" placeholder="Фамилия Имя Отчество *">
+                </div>
+                <div class="div_form">
+                    <input class="form__field" name="phone" placeholder="Телефон *">
+                </div>
+                <div class="div_form">
+                    <input class="form__field" name="email" placeholder="Email">
+                </div>
+            </div>
+                <div class="div_button">
+                    <button class="form__button" type="submit">ОТПРАВИТЬ ЗАПРОС</button>
+                </div>
+        </form>
+    </div>
+    <p class="msg none">Lorem ipsum dolor sit amet.</p>
+    <script type="text/javascript" src="assets/js/jquery-3.4.1.min.js"></script>
+    <script src="assets/js/script.js"></script>
+</body>
+</html>
